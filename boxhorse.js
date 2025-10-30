ccb44831c05e3ba2750b9741143d9bb153ce62c0
Gabriel de Souza Simplício <gabrieeel.08@gmail.com>
2025-10-29 22:16:06 -0300

    boxhorse

---
+++ boxhorse.js
@@ -0,0 +1,46 @@
+use("box_horse")
+
+// db.funcionario.insertMany([
+// {nome:"Mariana Alves Costa", cargo:"Veterinária", data_adm:"2022-03-15", contato:"(11) 98342-1159"},
+// {nome:"Rafael Pereira dos Santos", cargo:"Veterinário", data_adm:"2021-07-22", contato:"(21) 99741-8870"},
+// {nome:"Bianca Souza Oliveira", cargo:"Veterinária", data_adm:"2023-01-10", contato:"(31) 98522-3346"},
+// {nome:"Lucas Henrique Lima", cargo:"Veterinário", data_adm:"2020-11-05", contato:"(41) 99213-7784"},
+// {nome:"Fernanda Duarte Ribeiro", cargo:"Veterinária", data_adm:"2024-02-19", contato:"(51) 99108-6522"},
+
+// {nome:"Patrícia Gomes Andrade", cargo:"Secretária", data_adm:"2023-06-01", contato:"(62) 99812-4300"},
+// {nome:"Juliana Cardoso Pinto", cargo:"Secretária", data_adm:"2024-07-08", contato:"(85) 99943-1270"},
+
+// {nome:"Thiago Nascimento Barros", cargo:"Atendente", data_adm:"2021-12-17", contato:"(71) 98567-2891"},
+// {nome:"Camila Freitas Menezes", cargo:"Atendente", data_adm:"2020-05-30", contato:"(81) 99612-5534"},
+// {nome:"André Luiz Pacheco", cargo:"Atendente", data_adm:"2018-09-14", contato:"(91) 98234-7695"},
+
+// {nome:"João Victor Martins", cargo:"Caixa", data_adm:"2019-08-09", contato:"(61) 98455-9013"},
+
+// ])
+
+// db.cliente.insertMany([
+// {nome:"Carlos Eduardo Nunes", contato:"(11) 98234-7791", cpf_cnpj:"321.456.789-00", tipo_cliente:"PF", endereco:"Rua das Acácias, 125 - Jardim Paulista, São Paulo/SP", complemento:"Apto 12B"},
+// {nome:"Fazenda Boa Esperança LTDA", contato:"(12) 3945-8812", cpf_cnpj:"15.234.789/0001-65", tipo_cliente:"PJ", endereco:"Rodovia SP-062, Km 45 - Zona Rural, Taubaté/SP", complemento:"Sede"},
+// {nome:"Ana Beatriz Ferreira", contato:"(21) 99761-4420", cpf_cnpj:"287.115.940-02", tipo_cliente:"PF", endereco:"Rua das Laranjeiras, 89 - Laranjeiras, Rio de Janeiro/RJ", complemento:"Casa"},
+// {nome:"Fazenda Bela Vista ME", contato:"(31) 3344-2299", cpf_cnpj:"43.987.102/0001-21", tipo_cliente:"PJ", endereco:"Estrada Municipal, 1840 - Zona Rural, Belo Horizonte/MG", complemento:"Galpão 02"},
+// {nome:"João Paulo Mendes", contato:"(41) 99502-8841", cpf_cnpj:"118.904.623-59", tipo_cliente:"PF", endereco:"Rua XV de Novembro, 450 - Centro, Curitiba/PR", complemento:"Apto 501"},
+// {nome:"Maria Clara Barbosa", contato:"(51) 99177-6032", cpf_cnpj:"075.224.618-09", tipo_cliente:"PF", endereco:"Rua Bento Gonçalves, 220 - Centro, Porto Alegre/RS", complemento:"Casa dos fundos"},
+// {nome:"Fazenda São Francisco LTDA", contato:"(85) 3298-4432", cpf_cnpj:"09.552.884/0001-87", tipo_cliente:"PJ", endereco:"Rodovia CE-040, Km 12 - Meireles, Fortaleza/CE", complemento:"Armazém"},
+// {nome:"Gabriel Antunes Rocha", contato:"(62) 99855-2207", cpf_cnpj:"214.882.305-70", tipo_cliente:"PF", endereco:"Rua 10, 312 - Setor Oeste, Goiânia/GO", complemento:"Bloco C, Apto 303"},
+// {nome:"Associação Amigos dos Animais", contato:"(91) 3234-6721", cpf_cnpj:"48.223.117/0001-98", tipo_cliente:"PJ", endereco:"Av. Nazaré, 640 - Batista Campos, Belém/PA", complemento:"Sede"},
+// {nome:"Juliana Pereira Lima", contato:"(81) 98732-5519", cpf_cnpj:"402.771.988-12", tipo_cliente:"PF", endereco:"Rua da Aurora, 35 - Santo Amaro, Recife/PE", complemento:"Casa 2"}
+// ])
+
+// db.animal.insertMany([
+// {nome:"Rex", especie:"Cachorro", data_nascimento:"2021-05-10", cpf_cnpj:"321.456.789-00"},
+// {nome:"Tornado", especie:"Boi", data_nascimento:"2019-03-22", cpf_cnpj:"15.234.789/0001-65"},
+// {nome:"Mimi", especie:"Gato", data_nascimento:"2022-09-14", cpf_cnpj:"287.115.940-02"},
+// {nome:"Estrela", especie:"Vaca", data_nascimento:"2018-07-03", cpf_cnpj:"43.987.102/0001-21"},
+// {nome:"Thor", especie:"Cachorro", data_nascimento:"2020-11-19", cpf_cnpj:"118.904.623-59"},
+// {nome:"Nina", especie:"Gato", data_nascimento:"2021-08-25", cpf_cnpj:"075.224.618-09"},
+// {nome:"Caramelo", especie:"Porco", data_nascimento:"2020-09-28", cpf_cnpj:"09.552.884/0001-87"},
+// {nome:"Max", especie:"Cachorro", data_nascimento:"2021-07-30", cpf_cnpj:"214.882.305-70"},
+// {nome:"Pérola", especie:"Ovelha", data_nascimento:"2020-06-17", cpf_cnpj:"48.223.117/0001-98"},
+// {nome:"Lili", especie:"Gato", data_nascimento:"2023-06-11", cpf_cnpj:"402.771.988-12"}
+// ])
+
